13588549b5ee010a7ad5d6e2c562e1828431f4b5
lamvh <vhlam1997@gmail.com>
2024-07-31 23:43:32 +0700

    init waitlist and integrate Posthog and Turnstile

---
+++ src/app/waitlist/page.tsx
@@ -0,0 +1,98 @@
+"use client"
+import { Header } from "@/components/header"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Turnstile } from "@marsidev/react-turnstile"
+import { FormEvent, useState } from "react"
+import { usePostHog } from "posthog-js/react"
+
+export default function WaitlistPage() {
+  const posthog = usePostHog()
+  const turnstileSiteKey = process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY
+  const [verifiedTurnstitle, setVerifiedTurnstitle] = useState(false)
+  const [showTurnstitle, setShowTurnstitle] = useState<
+    "invisible" | "normal" | "compact" | "auto" | undefined
+  >("invisible")
+  const [formData, setFormData] = useState<{ name: string; email: string }>({
+    name: "",
+    email: "",
+  })
+
+  const onChange = (event: any) => {
+    const { name, value } = event.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    posthog?.capture("waitlist_user_added", {
+      properties: formData,
+      distinctId: formData?.email,
+    })
+  }
+
+  return (
+    <>
+      <Header />
+      <section className="flex flex-col h-screen justify-center items-center ">
+        <div className="flex flex-col justify-center items-center p-5 bg-inherite md:w-1/2 w-full">
+          <p className="bg-gradient-to-r from-zinc-100 to-zinc-700 text-transparent bg-clip-text p-5 md:text-4xl text-4xl font-bold text-center">
+            Join The Waitlist 6pm Today!
+          </p>
+
+          <p className="text-zinc-500 md:text-md sm:text-sm text-center">
+            Be among the first to experience the revolutionary AI Financial
+            Assistant that will transform the way you manage your expenses and
+            budgets.
+          </p>
+
+          <form
+            onSubmit={onSubmit}
+            className="flex flex-col items-center w-full py-5 space-y-2"
+          >
+            <Input
+              name="name"
+              required={true}
+              type="text"
+              placeholder="Your full name"
+              className="w-full md:w-1/2 bg-inherite"
+              onChange={onChange}
+            />
+            <Input
+              name="email"
+              onChange={onChange}
+              required={true}
+              type="email"
+              placeholder="Your email address"
+              className="w-full md:w-1/2 bg-inherite"
+            />
+            {turnstileSiteKey && (
+              <Turnstile
+                className="w-full md:w-1/2 justify-self-center	"
+                options={{ size: showTurnstitle }}
+                siteKey={turnstileSiteKey}
+                onError={() => {
+                  setShowTurnstitle("normal")
+                }}
+                onBeforeInteractive={() => {
+                  setShowTurnstitle("normal")
+                }}
+                onSuccess={() => {
+                  setVerifiedTurnstitle(true)
+                }}
+              />
+            )}
+            <Button
+              type="submit"
+              disabled={!verifiedTurnstitle}
+              className="rounded-md md:w-1/4 w-1/3 text-xs"
+            >
+              Join Waitlist
+            </Button>
+          </form>
+        </div>
+      </section>
+    </>
+  )
+}
