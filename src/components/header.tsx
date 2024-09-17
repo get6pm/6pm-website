81d5071eae0bb5de8f8f32b327c700d70c8d1104
lamvh <vhlam1997@gmail.com>
2024-09-17 11:12:10 +0700

    feat(web): Homepage - update gap-3 icon on header logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,7 +9,7 @@ export function Header() {
 			<div className="fixed border-b w-full border-[#A8AAB2]/[.25] h-16 top-0 backdrop-blur-xl" />
 			<header className="fixed flex max-w-4xl mx-auto overflow-hidden z-50 h-12 saturate-150 top-2 left-4 right-4">
 				<div className="flex mx-auto justify-between items-center w-full">
-					<Link href="/" className="flex items-center gap-2">
+					<Link href="/" className="flex items-center gap-3">
 						<img src="/logo.svg" alt="6pm" className="h-9 w-9" />
 						<div className="text-foreground text-xl font-medium">6pm</div>
 					</Link>
